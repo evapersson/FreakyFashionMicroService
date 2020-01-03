3ec17df64de2768f02ca4f08f3258955326a88de
Eva Persson <persson_eva@outlook.com>
2020-01-03 15:28:16 +0100

    Add personcontroller

---
+++ src/Services/Catalog/Catalog.API/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.Data;
+using Catalog.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+
+        private readonly ILogger<PersonController> _logger;
+        private readonly CatalogContext _context;
+
+        public PersonController(CatalogContext context, ILogger<PersonController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return _context.Person.ToList();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+        public CatalogContext(DbContextOptions<CatalogContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
